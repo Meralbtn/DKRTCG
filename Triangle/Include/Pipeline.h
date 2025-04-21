5fbc380afd1788946442b3eb80b26604c6ee2ab3
Meralbtn <fireklore@gmail.com>
2025-04-21 21:04:46 +0800

    Add files via upload

---
+++ Triangle/Include/Pipeline.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include <vulkan/vulkan.h>
+#include <string>
+#include <vector>
+
+
+namespace PR_BASE
+{
+    class APPEnter;
+
+
+    class Pipeline
+    {
+    public:
+        Pipeline();
+        ~Pipeline();
+        
+        void destroyPipe(const VkDevice& device);
+        void createRenderPass( APPEnter* enter);
+        void createPipeline( APPEnter* enter);
+        VkPipeline pipeline;
+        VkPipelineLayout pipelineLayout;
+        VkShaderModule vertShaderModule;
+        VkShaderModule fragShaderModule;
+        VkRenderPass renderPass;
+    private:
+        
+        static std::vector<char> loadShaderFiles(const std::string& filePath);
+        VkShaderModule createShaderModule(const std::vector<char>& code, const VkDevice& device);
+        
+    };
+}
---
+++ Triangle/Source/PRWindow.cpp
@@ -0,0 +1,35 @@
+﻿#include <PRWindow.h>
+
+namespace PR_BASE
+{
+    PRWindow::PRWindow(const uint32_t& width,const uint32_t& height,const std::string& title):width(width),height(height)
+    ,windowTitle(title)
+    {
+    
+    }
+
+    PRWindow::~PRWindow()
+    {
+        glfwDestroyWindow(window);
+    }
+
+    void PRWindow::initWindow()
+    {
+        //初始化窗口
+        glfwInit();
+        //禁止opengl
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window=glfwCreateWindow(width,height,windowTitle.c_str(),NULL,NULL);
+    }
+
+    GLFWwindow* PRWindow::getWindow()
+    {
+        return window;
+    }
+    void PRWindow::destroywindow()
+    {
+        glfwDestroyWindow(window);
+    }
+}
